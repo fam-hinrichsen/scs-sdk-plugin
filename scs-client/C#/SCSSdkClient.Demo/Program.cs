e0d92ad32debe70d45082cba0467e7be47920094
FlHinrichsen <florian@fam-hinrichsen.de>
2019-03-28 15:48:46 +0100

    enhance logging

--- scs-client/C#/SCSSdkClient.Demo/Program.cs
+++ scs-client/C#/SCSSdkClient.Demo/Program.cs
@@ -8,15 +8,18 @@ namespace SCSSdkClient.Demo
 {
     static class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
-    {
+        {
+            log.Info("Demo application started.");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SCSSdkClientDemo());
+            log.Info("Demo application stopped.");
         }
     }
 }
